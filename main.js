898fdc51d4eebe9587c21a9a2c81ed9cbaac601c
dele <eshmurodovsardor714@gmail.com>
2023-03-09 16:35:20 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,111 @@
+//Javascript nima ?
+
+//bu dinamik va interaktiv web ilovalarni yaratsish uchun ishlatiladigan mashxur dasturlash tilidir
+
+//alert() ?
+
+//<button onclick="alert('Salom, dunyo!')">Bosing</button> bu alert () shunday funksiya ekanki ochiq oynaga yoki oyna bolmagan joyda yopiq yorliqlar uchun ishlatiladi
+
+//let name = prompt("What is your name?");
+//alert("Hello, " + name + "!");
+
+//let userInput = prompt('Sizning ismingiz nima?');
+
+//let result = confirm("Haqiqatdan ham ochirishni istaysizmi?");
+
+
+//document.write("<h1>Salom bu men   , kechqurun dars qiluvchu dasturchi</h1>");
+
+
+//document.write("<h2>Hammaga alangali salomlar bolsin ");
+
+//let el = document.getElementById("example");
+//el.innerHTML = "<h1>Hello, world!</h1>";
+
+
+//let firstName = 'John';
+//let lastName = 'Doe';
+
+//template literal example
+//console.log(`My name is ${firstName} ${lastName}.`);
+
+//let a = 5;
+//let b = 7;
+//console.log(`The sum of ${a} and ${b} is ${a + b}.`);
+
+//boolean - har qandey malumotlar uchun ishlatialdi
+//let isTrue = true;
+//let isFalse = false;
+//console.log(isTrue); 
+//console.log(isFalse)
+
+//number elementlari uchun
+//let num = 10;
+//console.log(num);
+
+
+//string tipidagi elemetlari uchun ishlatialdi
+//let str = "Salom, dunyo!";
+//console.log(str); // Salom, dunyo
+
+
+//object misoli haar qandey turlarda malumotlar uchun ishlatiladi
+
+//let obj = { name: "John", age: 25 , surname:"JOnson"   };
+//console.log(obj); // { name: 'John', age: 25 }
+
+// let myVariable = null;
+// console.log(myVariable); 
+
+//let myVariable;
+//console.log(myVariable); 
+
+// let mySymbol = Symbol('hello Javascript');
+// console.log(typeof mySymbol); 
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+//https://javascript.info/strict-mode
+//https://www.w3schools.com/js/js_strict.asp
+
+
+// function myFunction() {
+//     "use strict";
+//     let x = 200;
+//     console.log(x);
+//   }
+//   myFunction();
+
+
+//   function myFunction(){
+//     "use strict";
+//     let x=100;
+//     console.log(x);
+
+//   }
+
+//   myFunction();
+
+// let x = 5;
+// x += 3; // x = x + 3
+// console.log(x); // 8
+
+// x o'zgaruvchisini y ga bo'lganda qoldiqni aniqlaydi va natijani x o'zgaruvchisiga yuklaydi`
+
+// let b = 7;   // = operatori
+// b %= 2;      // %= operatori
+// console.log(b);  //
+
+
+
+// console.log(Math.random()); // 0 dan 1 gacha bo'lgan tasodifiy son
+
+// let num1 = "42";
+// console.log(typeof num1);   
+// let num2 = Number(num1);
+// console.log(typeof num2)
+
+
+let num1 ="65"
+console.log(typeof num1);
+
+let num2 =Number(num1)
